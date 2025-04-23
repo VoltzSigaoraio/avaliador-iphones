e307bbb9563576ae6a384ce3141bded2d019f78b
Your Name <jgmartuchelli@hotmail.com>
2025-04-23 15:30:26 -0300

    Corrige roteamento com React Router e rotas para cliente e admin

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App'; // Dashboard admin
-import AvaliacaoCliente from './FormularioCliente'; // Novo formulário
+import App from './App';
+import AvaliacaoCliente from './FormularioCliente';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/admin" element={<App />} />
-      <Route path="/" element={<AvaliacaoCliente />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/admin" element={<App />} />
+        <Route path="/" element={<AvaliacaoCliente />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
