5299f8bde43d09e4871a25fc9b21803d953afa7d
Your Name <jgmartuchelli@hotmail.com>
2025-04-23 15:21:19 -0300

    Implementa rotas para admin e cliente com formulário separado

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App'; // Dashboard admin
+import AvaliacaoCliente from './FormularioCliente'; // Novo formulário
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+  <BrowserRouter>
+    <Routes>
+      <Route path="/admin" element={<App />} />
+      <Route path="/" element={<AvaliacaoCliente />} />
+    </Routes>
+  </BrowserRouter>
+);
